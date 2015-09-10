27f405e1c4e7536fe56e19babf2e4abab861dfcf
Mark Lotts <mlotts@gmail.com>
2015-09-10 03:31:06 -0400

    working version with infinite looping; possible strangeness with little pre-emption?

--- boundedBuffer/bbuffer.c
+++ boundedBuffer/bbuffer.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 typedef struct queue NumQueue;
 
@@ -10,7 +11,6 @@ struct queue
 	int size;
 	int nums[10];
 };
-
 
 //global variables
 NumQueue myQueue;
@@ -44,61 +44,78 @@ void shiftQueue(NumQueue* aQueue)
 
 void *produce()
 {
-	printf("Producer thread running...\n");
-
-	pthread_mutex_lock(&mutex);
-	while(myQueue.size >= maxSize)
+	int i;
+	while(1)
 	{
-		pthread_cond_wait(&upc,&mutex);
-	}
+		printf("Producer thread running...\n");
+		pthread_mutex_lock(&mutex);
+		printf("Producer thread has the lock...\n");
+		while(myQueue.size >= maxSize)
+		{
+			printf("Producer thread waiting...\n");
+			pthread_cond_wait(&upc,&mutex);
+			printf("Producer thread awakens and receives lock...\n");
+		}
 
-	int r = rand() % 10;
-	myQueue.nums[myQueue.size] = r;
-	printf("Added item %d to queue at position %d\n",r,myQueue.size);
-	myQueue.size += 1;	
+		int r = rand() % 10;
+		myQueue.nums[myQueue.size] = r;
+		printf("Added %d to queue at position %d\n",r,myQueue.size);
+		myQueue.size += 1;	
 	
-	printQueue(&myQueue);
+		printQueue(&myQueue);
 
-	pthread_mutex_unlock(&mutex);
-	pthread_cond_signal(&downc);
+		printf("Producer thread releasing lock and signaling...\n");
+		pthread_mutex_unlock(&mutex);
+		pthread_cond_signal(&downc);
+
+		//sleep(rand()%2);
+	}
 }
 
 void *consume()
 {
-	printf("Consumer thread\n");
-	pthread_mutex_lock(&mutex);
-	while(myQueue.size <= 0)
+	int i;
+	while(1)
 	{
-		pthread_cond_wait(&downc,&mutex);
+		printf("Consumer thread running...\n");
+		pthread_mutex_lock(&mutex);
+		printf("Consumer thread has the lock...\n");
+		while(myQueue.size <= 0)
+		{
+			printf("Consumer thread waiting...\n");
+			pthread_cond_wait(&downc,&mutex);
+			printf("Consumer thread awakens and receives lock...\n");
+		}
+
+		//consume item from front of queue
+		printf("Consumed %d from front of queue!\n",myQueue.nums[0]);
+
+		//need to shift items in queue to the front	
+		shiftQueue(&myQueue);
+		myQueue.size -= 1;
+		printQueue(&myQueue);
+
+		printf("Consumer thread releasing lock and signaling...\n");
+		pthread_mutex_unlock(&mutex);
+		pthread_cond_signal(&upc);
+
+		//sleep(rand()%2);
 	}
-
-	//consume item from front of queue
-	printf("Consumed %d from front of queue!\n",myQueue.nums[0]);
-	myQueue.size -= 1;
-
-	//need to shift items in queue to the front	
-	shiftQueue(&myQueue);
-	printQueue(&myQueue);
-
-	pthread_mutex_unlock(&mutex);
-	pthread_cond_signal(&upc);
 }
 
 int main(int argc, char **argv)
 {
 	srand(time(NULL));
 	pthread_t consumer, producer;
-	//pthread_attr_t attr;
-	//size_t stacksize;
-	//struct argument a1, a2;
 	myQueue.size = 0;
 	printf("\n");
-	pthread_create(&producer, NULL, produce, NULL);//(void *)&a1);
-	//pthread_create(&consumer, NULL, consume, NULL);//(void *)&a2);
+
+	pthread_create(&producer, NULL, produce, NULL);
+	pthread_create(&consumer, NULL, consume, NULL);
 	
 	pthread_join(producer, NULL);
-	//pthread_join(consumer, NULL);
-	//printf("%d\n", count);
+	pthread_join(consumer, NULL);
+
 	printf("Program finished running!\n");
 }
 
